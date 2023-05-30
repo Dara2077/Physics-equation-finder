b5f55cea1430f89a4df4c5823f6697b387f220a7
dara2077 <dbenyacar1@gmail.com>
2023-05-30 09:28:18 -0400

    Work from 05.30.23

--- src/Find.java
+++ src/Find.java
@@ -9,21 +9,21 @@ public class Find{
         equations.add("Dx = vit + 1/2at^2");
         equations.add("vf = vi + at");
         equations.add("vf^2 = vi^2 + 2aDx");
-        equations.add("p- = mv-");
-        equations.add("Dp = F-t");
+        equations.add("p = mv");
+        equations.add("J = Ft");
         equations.add("K = 1/2mv^2");
-        equations.add("DU = mgDh");
+        equations.add("U = mgh");
         equations.add("DE = Fdcosth = W");
         equations.add("ac = v^2/r");
         equations.add("Dx* = wi + 1/2a*t^2");
-        equations.add("Dw = at");
+        equations.add("Dw = a*t");
         equations.add("T = 2pi/w");
         equations.add("x = Acos(2pift)");
         equations.add("a- = T*net/I");
         equations.add("T* = Fr");
         equations.add("L = Iw");
         equations.add("DL = T*t");
-        equations.add("|Fs| = k|x-|");
+        equations.add("|Fs| = k|x|");
         equations.add("Us = 1/2kx^2");
         equations.add("Ts = 2pi√(mk)");
         equations.add("Tp = 2pi√(t/g)");
@@ -53,15 +53,7 @@ public class Find{
             }
         }
         finalEquations = ret;
-        print();
         return ret;
     }
 
-    private void print(){
-        System.out.println();
-        for (int i = 0; i < finalEquations.size(); i ++){
-            int num = i + 1;
-            System.out.println(num + ". " + finalEquations.get(i));
-        }
-    }
 }
--- src/SecondPage.java
+++ src/SecondPage.java
@@ -11,7 +11,12 @@ public class SecondPage extends JFrame implements ActionListener, KeyListener {
     private JTextArea textArea1;
     private JPanel panel2;
     private JButton resetButton;
+    private JTextArea textArea2;
+    private JTextField numberToBeRemovedTextField;
+    private JTextField textField2;
+    private JButton removeButton;
     private ArrayList<String> vars = new ArrayList<>();
+
 
     public SecondPage(){
         createUIComponents();
@@ -20,25 +25,43 @@ public class SecondPage extends JFrame implements ActionListener, KeyListener {
     private void createUIComponents() {
         setContentPane(panel2);
         setTitle("My GUI");
-        setSize(600, 500);
+        setSize(700, 350);
         setLocation(450, 300);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
         submitButton.addActionListener(this);
         resetButton.addActionListener(this);
+        removeButton.addActionListener(this);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         Object source = e.getSource();
         JButton but = (JButton) source;
-        if (but.getText().equals("Next")){
+        if (but.getText().equals("Input")){
             vars.add(textField1.getText());
+            textArea2.setText("");
+            for (int i = 0; i < vars.size(); i ++ ){
+                int num = i + 1;
+                textArea2.append(num + ". " + vars.get(i) + "\n");
+            }
             addEquations();
             textField1.setText("");
         }
+        else if (but.getText().equals("Remove")){
+            int num = Integer.parseInt(textField2.getText());
+            vars.remove(num -1);
+            textArea2.setText("");
+            for (int i = 0; i < vars.size(); i ++ ){
+                int num1 = i + 1;
+                textArea2.append(num1 + ". " + vars.get(i) + "\n");
+            }
+            textField2.setText("");
+            addEquations();
+        }
         else{
-            textArea1.setText("");
+            textArea1.setText("Variables cleared");
+            textArea2.setText("");
             vars.clear();
         }
     }
